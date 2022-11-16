b599e9c755f64facf32a74f47f181d2efa742f45
ShawnLemuelDabi <2001401@sit.singaporetech.edu.sg>
2022-11-16 11:16:48 +0800

    Updated deploy.sh

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://192.168.1.104";
+	String url = "http://localhost:8080";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
